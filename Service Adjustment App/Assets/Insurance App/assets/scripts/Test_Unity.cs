d7fdb39152b0498360136cc8841b494fc275a3cd
BakrAlqassab <alqassab31@gmail.com>
2019-06-24 10:41:38 +0300

    test 3
    Co-Authored-By: bakralqassab <bakralqassab@users.noreply.github.com>

--- Service Adjustment App/Assets/Insurance App/assets/scripts/Test_Unity.cs
+++ Service Adjustment App/Assets/Insurance App/assets/scripts/Test_Unity.cs
@@ -7,7 +7,7 @@ public class Test_Unity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Agree and Disagree 
     }
 
     // Update is called once per frame
